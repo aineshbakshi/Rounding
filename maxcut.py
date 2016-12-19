733b19567b495eff72eb3ab36ad61d7525b183ed
Poseidon <aineshbakshi@gmail.com>
2016-12-19 07:17:06 -0500

    reformulated the IP according to https://www.cs.cmu.edu/afs/cs.cmu.edu/academic/class/15859-f11/www/notes/lecture10.pdf

--- maxcut.py
+++ maxcut.py
@@ -1,57 +1,51 @@
 from gurobipy import *
-import networkx
+import networkx as nx
 from networkx.generators.random_graphs import *
 
-nodes=4
+nodes=5
 
 #Create a random graph
-#G = erdos_reyni_graph(nodes, 0.5, seed=None, directed=False)
-G=[[0 for x in range(nodes)]for y in range(nodes)]
-G[0][1]=1
-G[0][2]=1
-G[0][3]=1
-G[1][0]=1
-G[1][3]=1
-G[2][0]=1
-G[2][3]=1
-G[3][0]=1
-G[3][1]=1
-G[3][2]=1
-
+G = erdos_renyi_graph(nodes, 0.5, seed=None, directed=False)
+print G.edges()
 
 #Max cut Integer Program
 
 m = Model("maxcut")
 
 #Create Variables
-var=[]
+node_var=[]
+edge_var={}
 
 for i in range(nodes):
-    var.append(m.addVar(vtype=GRB.INTEGER,lb=-1.0, ub=1.0))
+    node_var.append(m.addVar(vtype=GRB.BINARY, name='v'+str(i)))
 
-#Constraints: x_i , x_j = {-1, 1} for all i, j in [n]
-#for i in range(nodes):
-#    m.addConstr(var[i], GRB.GREATER_EQUAL, 0.5)
-#    m.addConstr(var[i], GRB.LESS_EQUAL, -0.5)
+for edge in G.edges_iter():
+    edge_var[(edge[0],edge[1])]=m.addVar(vtype=GRB.BINARY)
 
-#Objective: Max_x (1/4)( Sum^n_1 Sum^n_1 w_{i,j}(1 - x_i*x_j))
+
+#Constraints: z
+#1.z_e <= x_u + x_v
+#2.z_e <= 2 - (x_u + x_v)
+
+for edge in G.edges_iter():
+    m.addConstr(edge_var[edge], GRB.LESS_EQUAL, node_var[edge[0]]+node_var[edge[1]]     )
+    m.addConstr(edge_var[edge], GRB.LESS_EQUAL, 2-(node_var[edge[0]]+node_var[edge[1]]) )
+
+#Objective: Max Sigma_{e in E} z_e
 obj=QuadExpr()
-for i in range(nodes):
-    for j in range(nodes):
-        #if edge exists
-        if G[i][j]:
-            obj += (1-var[i]*var[j])
 
-obj = obj/4
+#Iterate through edges to build objective function
+for edge in G.edges_iter():
+    obj += edge_var[(edge[0],edge[1])]
 
 m.setObjective(obj, GRB.MAXIMIZE)
 
 #Solve
 m.optimize()
 
-#Write to file 
-m.write('maxcut.ip')
 
+#Write to file 
+m.write('opt.sol')
 
 
 
